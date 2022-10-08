ee6e2faeb51768960b58bed8c74e165fe10df373
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-08 17:45:11 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 function Home() {
+  const token = process.env.REACT_APP_GITHUB_TOKEN
   return (
     <div>
-      <h1 className='text-6xl'>Welcome</h1>
+      <h1 className='text-6xl'>Welcome {token} 
+      
+      </h1>
     </div>
   )
 }
