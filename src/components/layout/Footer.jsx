7527c735e09d59a88b51e6f3c870c180c377ad87
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-08 16:53:00 +0600

    updates

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 function Footer() {
+
+  const footerYear = new Date().getFullYear()
+
+
   return (
     <footer className='footer p-10 bg-gray-700 text-primary-content footer-center'>
     <div>
