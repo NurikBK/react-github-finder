c60e9ee7a52802a191c59654656e81fb1193d15f
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-09 10:45:41 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,30 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Notfound from './pages/Notfound';
 import Footer from './components/layout/Footer';
+import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<Notfound />} />
-            <Route path="/*" element={<Notfound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<Notfound />} />
+                <Route path="/*" element={<Notfound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
   );
 }
 
