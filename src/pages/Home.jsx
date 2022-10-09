e562dae808c1848b2546d87a65e541e48eb46dcc
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-09 10:46:18 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import UsersResults from '../components/users/UsersResults'
+import UserSearch from '../components/users/UserSearch'
+
 
 function Home() {
   const token = process.env.REACT_APP_GITHUB_TOKEN
   return (
     <>
+      <UserSearch />
       <UsersResults />
     </>
   )
