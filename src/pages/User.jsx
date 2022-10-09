a44ad3376ed3af081638e55747755711bea0c183
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-09 12:17:11 +0600

    Update User.jsx

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import {useContext, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import GithubContext from '../context/github/GithubContext'
 
 function User() {
+  const {user, getUser} = useContext(GithubContext)
+  const params = useParams()
+
+  useEffect(() => {
+    getUser(params.login)
+    // getUserRepo(params.login)
+  }, [])
+
+
   return (
-    <div>User</div>
+    <div>{user.login}</div>
   )
 }
 
