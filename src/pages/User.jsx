e1a730cfe0a927a35f980a4047924a16a140e7d9
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-09 17:33:03 +0600

    Update User.jsx

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,13 @@
+import {FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
 import {useContext, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import GithubContext from '../context/github/GithubContext'
+import Spinner from '../components/layout/Spinner'
+
+
 
 function User() {
-  const {user, getUser} = useContext(GithubContext)
+  const {user, getUser, isLoading} = useContext(GithubContext)
   const params = useParams()
 
   useEffect(() => {
@@ -11,9 +15,36 @@ function User() {
     // getUserRepo(params.login)
   }, [])
 
+  const {type, avatar_url, location, bio, blog, twitter_username, login, html_url, followers, following, public_repos, public_gists, hireable} = user
+
+  if(isLoading) {
+    return <Spinner />
+  }
 
   return (
-    <div>{user.login}</div>
+   <>
+    <div className="w-full mx-auto lg:w-10/12">
+      <div className="mb-4">
+        <Link to='/' className='btn btn-ghost'>Back to Search</Link>
+      </div>
+
+      <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+        <div className="custom-card-image mb-6 md:mb-0">
+          <div className="rounded-lg shadow-lx card image-full">
+            <figure>
+              <img src={avatar_url} alt="profile" />
+            </figure>
+            <div className="card-body justyfy-end">
+              <h2 className="card-title mb-0">
+                {}
+              </h2>
+              <p>{login}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </>
   )
 }
 
