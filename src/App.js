6b2390f20cfc5f289cfe8fc3c618a6839f04bf6b
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-09 11:19:50 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Notfound from './pages/Notfound';
-import Footer from './components/layout/Footer';
+import User from './pages/User';
 import { GithubProvider } from './context/github/GithubContext';
 import { AlertProvider } from './context/alert/AlertContext';
 
@@ -16,9 +18,11 @@ function App() {
             <Navbar />
 
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path='/user/:login' element={<User />} />
                 <Route path="/notfound" element={<Notfound />} />
                 <Route path="/*" element={<Notfound />} />
               </Routes>
