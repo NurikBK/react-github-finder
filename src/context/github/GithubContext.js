9033ed659367e8f32ed04541038e0ef2c600b702
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-10 18:52:02 +0600

    Update GithubContext.js

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,75 +1,28 @@
-import { createContext, useReducer } from 'react';
-import githubReducer from './GithubReducer';
+import { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducer'
 
-const GithubContext = createContext();
+const GithubContext = createContext()
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
-export function GithubProvider({ children }) {
+export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
-    isLoading: false,
-  };
-
-  const [state, dispatch] = useReducer(githubReducer, initialState);
-
-  async function searchUsers(text) {
-    setIsLoading();
-
-    const params = new URLSearchParams({ q: text });
-
-    const res = await fetch(`${GITHUB_URL}/search/users?${params}`);
-    const { items } = await res.json();
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    });
+    repos: [],
+    loading: false,
   }
 
-  async function getUser(login) {
-    setIsLoading();
-
-    const res = await fetch(`${GITHUB_URL}/search/users?q=${login}`);
-
-    if (res.status === 404) {
-      window.location = '/notfound';
-    } else {
-      const data = await res.json();
-      dispatch({
-        type: 'GET_USER',
-        payload: data.items[0],
-      });
-    }
-  }
-
-  // set isLoading
-  function setIsLoading() {
-    dispatch({ type: 'SET_LOADING' });
-  }
-
-  function clearUsers() {
-    dispatch({
-      type: 'CLEAR_USERS',
-    });
-  }
+  const [state, dispatch] = useReducer(githubReducer, initialState)
 
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        isLoading: state.isLoading,
-        searchUsers,
-        getUser,
-        clearUsers,
+        ...state,
+        dispatch,
       }}
     >
       {children}
     </GithubContext.Provider>
-  );
+  )
 }
 
-export default GithubContext;
+export default GithubContext
