08b7beac0dd983efa1da936e0379197c0c42584a
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-10 18:51:53 +0600

    Update GithubReducer.js

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,31 +1,31 @@
-function githubReducer(state, action) {
+const githubReducer = (state, action) => {
   switch (action.type) {
     case 'GET_USERS':
       return {
         ...state,
         users: action.payload,
-        isLoading: false,
-      };
-    case 'GET_USER':
+        loading: false,
+      }
+    case 'GET_USER_AND_REPOS':
       return {
         ...state,
-        user: action.payload,
-        isLoading: false,
-      };
-
+        user: action.payload.user,
+        repos: action.payload.repos,
+        loading: false,
+      }
     case 'SET_LOADING':
       return {
         ...state,
-        isLoading: true,
-      };
+        loading: true,
+      }
     case 'CLEAR_USERS':
       return {
         ...state,
         users: [],
-      };
+      }
     default:
-      return state;
+      return state
   }
 }
 
-export default githubReducer;
+export default githubReducer
